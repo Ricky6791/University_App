46d4fedec7b76339b2fa6a4532d5844d75169f02
Ricardo Avila <ricky6791@gmail.com>
2017-09-28 21:48:15 -0600

    added authentication

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,3 +1,7 @@
 class Professor < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 	has_many :courses
 end
